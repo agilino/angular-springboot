50a2f68ec55f8147b63cf0408c8521a8940b56d6
ttlinh <totuanlinh@gmail.com>
2023-05-30 15:27:07 +0700

    first commit

---
+++ order-cinema-ticket/src/main/java/com/agilino/ordercinematicket/OrderCinemaTicketApplication.java
@@ -0,0 +1,13 @@
+package com.agilino.ordercinematicket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderCinemaTicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderCinemaTicketApplication.class, args);
+    }
+
+}
---
+++ order-cinema-ticket/src/test/java/com/agilino/ordercinematicket/OrderCinemaTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agilino.ordercinematicket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderCinemaTicketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
