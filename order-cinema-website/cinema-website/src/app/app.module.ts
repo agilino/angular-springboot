a5c3f1bcf9f0d22bca9c366cda9373dd92f32199
vinhho3010 <anhvinh30102001@gmail.com>
2023-06-02 16:52:01 +0700

    feat: add draft pick showtime

--- order-cinema-website/cinema-website/src/app/app.module.ts
+++ order-cinema-website/cinema-website/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PickShowtimeComponent } from './pick-showtime/pick-showtime.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PickShowtimeComponent
   ],
   imports: [
     BrowserModule,
