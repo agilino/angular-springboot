763495bc0fcdc23d326561a0ce7df0c849d55b05
vinhho3010 <anhvinh30102001@gmail.com>
2023-06-05 08:50:25 +0700

    chore: refactor

--- order-cinema-website/cinema-website/src/app/pick-showtime/pick-showtime.component.ts
+++ order-cinema-website/cinema-website/src/app/pick-showtime/pick-showtime.component.ts
@@ -6,7 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./pick-showtime.component.scss']
 })
 export class PickShowtimeComponent {
-  iSelected = false
+  selectedDay = {
+    day: "Th 2",
+    date: "20/10",
+  }
+  selectedCity = "Cần Thơ"
   dateMock = [
     {
       day: "Th 2",
@@ -18,23 +22,23 @@ export class PickShowtimeComponent {
     },
     {
       day: "Th 4",
-      date: "20/10",
+      date: "22/10",
     },
     {
       day: "Th 5",
-      date: "20/10",
+      date: "23/10",
     },
     {
       day: "Th 6",
-      date: "20/10",
+      date: "24/10",
     },
     {
       day: "Th 7",
-      date: "20/10",
+      date: "25/10",
     },
     {
       day: "CN",
-      date: "20/10",
+      date: "26/10",
     },
   ]
 
@@ -45,6 +49,7 @@ export class PickShowtimeComponent {
       titleEng: "Face Off 6: The Ticket Of Destiny · NC16 · 2h12' ",
       type:"2D Lồng Tiếng",
       showtimes: [ "9:00", "16:00", "18:00","20:00", "22:00", "24:00"  ],
+      department: "CGV Sense City",
     },
     {
       bannerUrl: "https://cdn.moveek.com/storage/media/cache/mini/643cd05155dc9009010254.jpeg",
@@ -52,15 +57,26 @@ export class PickShowtimeComponent {
       titleEng: "Face Off 6: The Ticket Of Destiny · NC16 · 2h12' ",
       type:"2D Lồng Tiếng",
       showtimes: [ "20:00", "22:00", "24:00" ],
+      department: "CGV Hùng Vương",
     },{
       bannerUrl: "https://cdn.moveek.com/storage/media/cache/mini/643cd05155dc9009010254.jpeg",
       title: "Lật Mặt: 48H",
       titleEng: "Face Off 6: The Ticket Of Destiny · NC16 · 2h12' ",
       type:"2D Lồng Tiếng",
       showtimes: [ "20:00", "22:00", "24:00" ],
+      department: "CGV Xuân Khánh",
     }
   ]
-  selectDay(){
-    this.iSelected = true;
+
+  city = ["Cần Thơ", "Đà Nẵng", "TP. Hồ Chí Minh"]
+  selectDay(item: any){
+    if(this.selectedDay.date!= item.date){
+      this.selectedDay = item;
+    }
+  }
+  selectCity(item: string){
+    if(this.selectedCity!=item){
+      this.selectedCity = item;
+    }
   }
 }
