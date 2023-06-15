ce39b82ca847c2d8aeecbeaee250ffff762691a0
Phuong Nguyen <ndphuong.agilino@gmail.com>
2023-06-15 16:24:06 +0700

    feat: fix status chair

--- order-cinema-ticket/src/main/java/com/agilino/ordercinematicket/repository/TicketRepository.java
+++ order-cinema-ticket/src/main/java/com/agilino/ordercinematicket/repository/TicketRepository.java
@@ -2,10 +2,16 @@ package com.agilino.ordercinematicket.repository;
 
 import com.agilino.ordercinematicket.model.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, UUID>{
+
+
+    @Query(value = "SELECT t FROM Ticket t WHERE t.chair.id IN (:chairIds)")
+    public List<Ticket> findAllTicketByChairs(List<UUID> chairIds);
 }
--- order-cinema-ticket/src/main/java/com/agilino/ordercinematicket/service/ChairService.java
+++ order-cinema-ticket/src/main/java/com/agilino/ordercinematicket/service/ChairService.java
@@ -7,29 +7,46 @@ import com.agilino.ordercinematicket.dto.chair.ChairDTO;
 import com.agilino.ordercinematicket.enums.ChairStatus;
 import com.agilino.ordercinematicket.mapper.AppMapper;
 import com.agilino.ordercinematicket.repository.ChairRepository;
+import com.agilino.ordercinematicket.repository.TicketRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ChairService {
     private final AppMapper appMapper;
     private final ChairRepository chairRepository;
-    public List<ChairDTO> getChairs(UUID departmentId, UUID timeId) {
-        List<ChairDTO> chairs = new ArrayList<>();
-        if (departmentId != null && timeId != null) {
-            chairs = chairRepository.findByDepartmentId(departmentId, timeId).stream()
-                    .map(appMapper::toDto).peek(item -> item.setStatus(ChairStatus.FREE))
-                    .toList();
-        }
-        return chairs;
-    }
 
-    public ChairDTO getChair(UUID id) {
-        return appMapper.toDto(chairRepository.findById(id).orElseThrow(()-> new NotFoundException("Chair not found")));
+    private final TicketRepository ticketRepository;
+
+    public List<ChairDTO> getChairs(UUID departmentId, UUID timeId) {
+        if (departmentId == null || timeId == null) {
+            return new ArrayList<>(); // Return empty list if either parameter is null
+        }
+
+        List<ChairDTO> chairs = chairRepository.findByDepartmentId(departmentId, timeId).stream().map(item -> {
+            ChairDTO dto = appMapper.toDto(item);
+            dto.setStatus(ChairStatus.FREE);
+            return dto;
+        }).toList();
+
+        Set<UUID> chairsExisting = ticketRepository.findAllTicketByChairs(chairs.stream().map(ChairDTO::getId).toList())
+                .stream()
+                .map(item -> item.getChair().getId())
+                .collect(Collectors.toSet());
+
+        chairs.forEach(item -> {
+            if (chairsExisting.contains(item.getId())) {
+                item.setStatus(ChairStatus.BOOKED);
+            }
+        });
+
+        return chairs;
     }
 }
